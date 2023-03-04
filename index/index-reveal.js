51e4ee1a2a67dd7f6cdadb171db711cc6890b02b
Kmzf777 <rafaelreisssilva034@gmail.com>
2023-03-04 12:32:12 -0300

    .

---
+++ index/index-reveal.js
@@ -0,0 +1,133 @@
+ScrollReveal().reveal(".movie-box:nth-child(1)", {
+    duration: 500,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+  ScrollReveal().reveal(".movie-box:nth-child(2)", {
+    duration: 580,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+  ScrollReveal().reveal(".movie-box:nth-child(3)", {
+    duration: 640,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+  ScrollReveal().reveal(".movie-box:nth-child(4)", {
+    duration: 710,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+  ScrollReveal().reveal(".movie-box:nth-child(5)", {
+    duration: 780,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+  ScrollReveal().reveal(".movie-box:nth-child(6)", {
+    duration: 840,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+
+ScrollReveal().reveal(".movie-breve:nth-child(1)", {
+    duration: 500,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+  ScrollReveal().reveal(".movie-breve:nth-child(2)", {
+    duration: 580,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+  ScrollReveal().reveal(".movie-breve:nth-child(3)", {
+    duration: 640,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+  ScrollReveal().reveal(".movie-breve:nth-child(4)", {
+    duration: 710,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+  ScrollReveal().reveal(".movie-breve:nth-child(5)", {
+    duration: 780,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+  ScrollReveal().reveal(".movie-breve:nth-child(6)", {
+    duration: 840,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+  });
+
+ScrollReveal().reveal(".info-area", {
+    duration: 500,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+});
+
+ScrollReveal().reveal(".social-btt:nth-child(1)", {
+    duration: 600,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+});
+
+ScrollReveal().reveal(".social-btt:nth-child(2)", {
+    duration: 800,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+});
---
+++ precos/precos-reveal.js
@@ -0,0 +1,26 @@
+ScrollReveal().reveal(".novidade-box:nth-child(1)", {
+    duration: 500,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+});
+
+ScrollReveal().reveal(".novidade-box:nth-child(2)", {
+    duration: 700,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+});
+
+ScrollReveal().reveal(".info-price", {
+    duration: 500,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.1
+
+});
