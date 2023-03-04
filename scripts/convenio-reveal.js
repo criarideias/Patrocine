e9057bf010f31902a67bd35497e2afaa50813002
Kmzf777 <99519621+Kmzf777@users.noreply.github.com>
2023-03-04 17:07:37 -0300

    .

---
+++ scripts/convenio-reveal.js
@@ -0,0 +1,27 @@
+ScrollReveal().reveal(".tittle", {
+    duration: 500,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+});
+
+ScrollReveal().reveal(".fazer-convenio", {
+    duration: 500,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+});
+
+ScrollReveal().reveal(".info-box", {
+    duration: 500,
+    origin: "bottom",
+    distance: "500px",
+    easing: "ease-in-out",
+    opacity: 0.3
+
+});
+
