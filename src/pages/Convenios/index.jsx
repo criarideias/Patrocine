2ca3d4096a3e33b5f27bb76a43300818d9a7db19
Henrique <henriqueferreira.fox@gmail.com>
2023-04-24 21:46:01 -0300

    finalização da página de convênios

--- src/pages/Convenios/index.jsx
+++ src/pages/Convenios/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./Convenios.css";
 import "./Convenios-Mobile.css";
 
-import AnuncioBomboniere from "../../assets/anuncio-bomboniere.jpeg";
+import CartaoConvenio from "../../assets/cartao-convenio.jpg";
 
 const Convenios = () => {
   return (
@@ -12,83 +12,41 @@ const Convenios = () => {
       <section className="convenio-info">
         <h2 className="tittle">O Que São?</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
-          necessitatibus similique quod, mollitia eveniet iure explicabo totam
-          officia reprehenderit veritatis ducimus perferendis enim earum et
-          laborum laboriosam voluptatem tenetur consequuntur! Lorem ipsum dolor
-          sit amet consectetur, adipisicing elit. Vitae necessitatibus similique
-          quod, mollitia eveniet iure explicabo totam officia reprehenderit
-          veritatis ducimus perferendis enim earum et laborum laboriosam
-          voluptatem tenetur consequuntur! Lorem ipsum dolor sit amet
-          consectetur, adipisicing elit. Vitae necessitatibus similique quod,
-          mollitia eveniet iure explicabo totam officia reprehenderit veritatis
-          ducimus perferendis enim earum et laborum laboriosam voluptatem
-          tenetur consequuntur! Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Vitae necessitatibus similique quod, mollitia
-          eveniet iure explicabo totam officia reprehenderit veritatis ducimus
-          perferendis enim earum et laborum laboriosam voluptatem tenetur
-          consequuntur! Lorem ipsum dolor sit amet consectetur, adipisicing
-          elit. Vitae necessitatibus similique quod, mollitia eveniet iure
-          explicabo totam officia reprehenderit veritatis ducimus perferendis
-          enim earum et laborum laboriosam voluptatem tenetur consequuntur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
-          necessitatibus similique quod, mollitia eveniet iure explicabo totam
-          officia reprehenderit veritatis ducimus perferendis enim earum et
-          laborum laboriosam voluptatem tenetur consequuntur!
+          Os convênios no cinema Patrocine são acordos realizados entre empresas
+          e o cinema, que permitem que seus colaboradores tenham acesso a preços
+          especiais e vantagens exclusivas na compra de ingressos e produtos.
+          Com os convênios, é possível economizar e ainda desfrutar de uma
+          experiência incrível no cinema, com opções de combos especiais e
+          cartão fidelidade para acumular pontos e trocar por ingressos ou
+          produtos.
         </p>
 
         <h2 className="tittle">Qual Suas Vantagens?</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
-          necessitatibus similique quod, mollitia eveniet iure explicabo totam
-          officia reprehenderit veritatis ducimus perferendis enim earum et
-          laborum laboriosam voluptatem tenetur consequuntur!Lorem ipsum dolor
-          sit amet consectetur, adipisicing elit. Vitae necessitatibus similique
-          quod, mollitia eveniet iure explicabo totam officia reprehenderit
-          veritatis ducimus perferendis enim earum et laborum laboriosam
-          voluptatem tenetur consequuntur! Lorem ipsum dolor sit amet
-          consectetur, adipisicing elit. Vitae necessitatibus similique quod,
-          mollitia eveniet iure explicabo totam officia reprehenderit veritatis
-          ducimus perferendis enim earum et laborum laboriosam voluptatem
-          tenetur consequuntur! Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Vitae necessitatibus similique quod, mollitia
-          eveniet iure explicabo totam officia reprehenderit veritatis ducimus
-          perferendis enim earum et laborum laboriosam voluptatem tenetur
-          consequuntur! Lorem ipsum dolor sit amet consectetur, adipisicing
-          elit. Vitae necessitatibus similique quod, mollitia eveniet iure
-          explicabo totam officia reprehenderit veritatis ducimus perferendis
-          enim earum et laborum laboriosam voluptatem tenetur consequuntur!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
-          necessitatibus similique quod, mollitia eveniet iure explicabo totam
-          officia reprehenderit veritatis ducimus perferendis enim earum et
-          laborum laboriosam voluptatem tenetur consequuntur!
+          As vantagens dos convênios no cinema Patrocine são diversas e vão
+          desde descontos em ingressos e combos especiais, até a possibilidade
+          de acumular pontos e trocar por ingressos ou produtos através do
+          cartão fidelidade. Além disso, as empresas que optam por fazer
+          convênios podem oferecer benefícios diferenciados aos seus
+          colaboradores, gerando mais engajamento e motivação no ambiente de
+          trabalho.
         </p>
 
         <h2 className="tittle">Como Fazer?</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
-          necessitatibus similique quod, mollitia eveniet iure explicabo totam
-          officia reprehenderit veritatis ducimus perferendis enim earum et
-          laborum laboriosam voluptatem tenetur consequuntur! Lorem ipsum dolor
-          sit amet consectetur, adipisicing elit. Vitae necessitatibus similique
-          quod, mollitia eveniet iure explicabo totam officia reprehenderit
-          veritatis ducimus perferendis enim earum et laborum laboriosam
-          voluptatem tenetur consequuntur! Lorem ipsum dolor sit amet
-          consectetur, adipisicing elit. Vitae necessitatibus similique quod,
-          mollitia eveniet iure explicabo totam officia reprehenderit veritatis
-          ducimus perferendis enim earum et laborum laboriosam voluptatem
-          tenetur consequuntur! Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Vitae necessitatibus similique quod, mollitia
-          eveniet iure explicabo totam officia reprehenderit veritatis ducimus
-          perferendis enim earum et laborum laboriosam voluptatem tenetur
-          consequuntur! Lorem ipsum dolor sit amet consectetur, adipisicing
-          elit. Vitae necessitatibus similique quod, mollitia eveniet iure
-          explicabo totam officia reprehenderit veritatis ducimus perferendis
-          enim earum et laborum laboriosam voluptatem tenetur consequuntur!
+          Fazer convênios no Patrocine é muito fácil e pode ser feito por
+          empresas que desejam oferecer benefícios aos seus colaboradores. Basta
+          entrar em contato com o cinema e verificar as opções de convênios
+          disponíveis, que geralmente incluem descontos em ingressos, combos
+          especiais e cartão fidelidade. As empresas também podem personalizar
+          os convênios de acordo com suas necessidades e definir as formas de
+          pagamento e utilização dos mesmos. Não perca a oportunidade de
+          oferecer vantagens exclusivas aos seus colaboradores e ainda
+          proporcionar momentos incríveis no Patrocine!
         </p>
         <button
           onClick={() => {
-            window.open("https://wa.me/553499505483");
+            window.open("https://wa.me/553431990422");
           }}
           className="fazer-convenio"
         >
@@ -97,7 +55,7 @@ const Convenios = () => {
       </section>
       <div className="info-box">
         <h2>Exemplo de Cartão de Convênio</h2>
-        <img src={AnuncioBomboniere} alt="patrocine cartão convênio" />
+        <img src={CartaoConvenio} alt="patrocine cartão convênio" />
       </div>
     </>
   );
