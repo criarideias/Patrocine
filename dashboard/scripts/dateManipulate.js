113528dda041bb2f7c9ac506da8d778c565f0d99
Jhonattas Henrique <jhonattas@soucriador.com>
2023-05-04 22:16:05 -0300

    faz o refactor de dashboard

--- dashboard/scripts/dateManipulate.js
+++
@@ -1,7 +0,0 @@
-/**
- * Take the difference between the dates and divide by milliseconds per day.
- * Round to nearest whole number to deal with DST.
- */
-export function datediff(first, second) {
-  return Math.round((second - first) / (1000 * 60 * 60 * 24));
-}
